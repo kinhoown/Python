f7d48c3912bf748886d723491ad48faf62f193f7
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2024-04-26 21:54:02 -0300

    Dia5
    att part2

--- Iniciando_na_programacao_com_python/Exercicio/Exercicio6_Aula_1-22.py
+++ Iniciando_na_programacao_com_python/Exercicio/Exercicio6_Aula_1-22.py
@@ -1,7 +1,24 @@
-nome = input('Digite seu nome: ')
-cont = 0
-nome_res = ''
-while len(nome) > cont:
-    nome_res = nome_res + (f'*{nome[cont]}')
-    cont +=1
-print(nome_res)
+# #atividade 1 aula 64 e 65
+
+# nome = input('Digite seu nome: ')
+# cont = 0
+# nome_res = ''
+# while len(nome) > cont:
+#     nome_res = nome_res + (f'*{nome[cont]}')
+#     cont +=1
+# print(nome_res)
+
+# #atividade 2 aula 66
+
+numero_1 = float(input('Numero: '))
+operador = input('Operador: ')
+numero_2 = float(input('Numero: '))
+
+if operador == '+':
+    print(numero_1 + numero_2)
+elif operador == '-':
+    print(numero_1 - numero_2)
+elif operador == '*':
+    print(numero_1 * numero_2)
+elif operador == '/':
+    print(numero_1 / numero_2)
