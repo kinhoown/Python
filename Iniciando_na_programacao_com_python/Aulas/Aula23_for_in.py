bfff9c3a607f366ba2d3f9e5d83ace0c6cbe8c44
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2024-04-28 23:27:26 -0300

    Dia7
    att

---
+++ Iniciando_na_programacao_com_python/Aulas/Aula23_for_in.py
@@ -0,0 +1,10 @@
+'''
+for in significa -> para todo , dentro de.
+ex: for 1 in 101011101:  -> para todo 1 dentro de 101011101: faça isso -->--v
+        print('Erick')
+'''
+
+nome = 'Erick'
+
+for letra in nome:
+    print(letra.upper())
---
+++ Iniciando_na_programacao_com_python/Aulas/Aula24_for_range.py
@@ -0,0 +1,11 @@
+'''
+range funciona com (star, stop, step).
+ex: renge(20) --> ta dizendo q o start é 0, o stop é 20 e o step é 1.
+renge(3,30) --> ta dizendo q o start é 3, o stop é 30 e o step é 1.
+renge(4,40,4) --> ta dizendo q o start é 4, o stop é 40 e o step é 4.
+'''
+
+numeros = range(10)
+
+for numero in numeros:
+    print(numero, numeros)
