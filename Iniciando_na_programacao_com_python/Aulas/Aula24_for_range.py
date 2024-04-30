900da8f44b4ff498a4037bd44aabf0750c7954e6
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2024-04-29 21:51:07 -0300

    Dia8
    att

--- Iniciando_na_programacao_com_python/Aulas/Aula24_for_range.py
+++ Iniciando_na_programacao_com_python/Aulas/Aula24_for_range.py
@@ -5,7 +5,7 @@ renge(3,30) --> ta dizendo q o start é 3, o stop é 30 e o step é 1.
 renge(4,40,4) --> ta dizendo q o start é 4, o stop é 40 e o step é 4.
 '''
 
-numeros = range(10)
+numeros = range(10, 1000, 10)
 
 for numero in numeros:
     print(numero, numeros)
---
+++ Iniciando_na_programacao_com_python/Exercicio/Exercicio8_Aula_1-24.py
@@ -0,0 +1,39 @@
+"""
+Faça um jogo para o usuário adivinhar qual
+a palavra secreta.
+- Você vai propor uma palavra secreta
+qualquer e vai dar a possibilidade para
+o usuário digitar apenas uma letra.
+- Quando o usuário digitar uma letra, você 
+vai conferir se a letra digitada está
+na palavra secreta.
+    - Se a letra digitada estiver na
+    palavra secreta; exiba a letra;
+    - Se a letra digitada não estiver
+    na palavra secreta; exiba *.
+Faça a contagem de tentativas do seu
+usuário.
+"""
+# palavra = input('Digite uma palavra: ').upper()
+# palavra_chave = len(palavra)*'*'
+
+# while True:
+#     if palavra_chave == palavra:
+#         palavra_chave = len(palavra)*'*'
+#         print('Você venceu!')
+#         break
+#     cont = 0
+#     letra = input('Digite uma letra: ').upper()
+#     if len(letra)>1:
+#         print('Digite apenas uma letra.')
+#     else:
+#         for i in palavra:
+#             if i == letra:
+#                 palavra_chave = palavra_chave[:cont]+i+palavra_chave[cont+1:]
+#                 cont+=1
+#                 print(f'Palavra formatada: {palavra_chave}')
+#                 break
+#             elif i == palavra[-1]:
+#                 print(f'Palavra formatada: {palavra_chave}')
+#             elif i != letra:
+#                 cont+=1
