ea0f8c486f536ad1cdf5bc598bde1e797f52c3e0
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2024-05-19 12:27:44 -0300

    Dia17
    atividade

---
+++ Curso_python_3_udemy/MODULO2_python_intermediario/Exercicio/Exercicio3_Aula_105-123.py
@@ -0,0 +1,24 @@
+'''
+sistema de perguntas e respostas
+'''
+
+perguntas = [
+    {
+        'pergunta' : 'Quanto é 2+2?',
+        'Opções'   : ['1', '2', '3', '4'],
+        'resposta' : '4',
+    },
+    {
+        'pergunta' : 'Quanto é 5*5?',
+        'Opções'   : ['25', '55', '10', '51'],
+        'resposta' : '25',
+    },
+    {
+        'pergunta' : 'Quanto é 10/2?',
+        'Opções'   : ['4', '5', '2', '1'],
+        'resposta' : '5',
+    },
+]
+
+for i in perguntas:
+    print(i)
