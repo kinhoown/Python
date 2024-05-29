56ed7bf9a34abf040f22033be3b905e21e63cc01
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2024-05-29 17:57:49 -0300

    Dia22

--- Curso_python_3_udemy/MODULO2_python_intermediario/Exercicio/Exercicio5_Aula_105-159.py
+++ Curso_python_3_udemy/MODULO2_python_intermediario/Exercicio/Exercicio5_Aula_105-159.py
@@ -1,7 +1,5 @@
-# copy, sorted, produtos.sort
+import copy
 # Exercícios
-# Aumente os preços dos produtos a seguir em 10%
-# Gere novos_produtos por deep copy (cópia profunda)
 produtos = [
     {'nome': 'Produto 5', 'preco': 10.00},
     {'nome': 'Produto 1', 'preco': 22.32},
@@ -10,12 +8,40 @@ produtos = [
     {'nome': 'Produto 4', 'preco': 69.90},
 ]
 
-# Ordene os produtos por nome decrescente (do maior para menor)
-# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
-
-# Ordene os produtos por preco crescente (do menor para maior)
-# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
-
-#for i in produtos:
-
-print(produtos.values())
+# 1 - Aumente os preços dos produtos a seguir em 10%
+'''
+for produto in produtos:
+    produto['preco'] = ((10 / 100) * produto['preco']) + produto['preco']
+    print(produto)
+'''
+# 2 - Gere novos_produtos por deep copy (cópia profunda)
+'''
+produtos_2 = copy.deepcopy(produtos)
+print(produtos_2)
+'''
+# 3 - Ordene os produtos por nome decrescente (do maior para menor)
+'''
+produtos.sort(key=lambda item: item['nome'], reverse=True)
+for produto in produtos:
+    print(produto)
+'''
+# 4 - Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+'''
+produtos_2 = copy.deepcopy(produtos)
+produtos_2.sort(key=lambda item: item['nome'])
+for produto in produtos_2:
+    print(produto)
+'''
+# 5 - Ordene os produtos por preco crescente (do menor para maior)
+'''
+produtos.sort(key=lambda item: item['preco'])
+for produto in produtos:
+    print(produto)
+'''
+# 6 - Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+'''
+produtos_2 = copy.deepcopy(produtos)
+produtos_2.sort(key=lambda item: item['preco'])
+for produto in produtos_2:
+    print(produto)
+'''
