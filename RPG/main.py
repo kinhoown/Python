91a318040ff7d1e2f6cadf13fb69c3720910d23f
Erick Alexandre Pessoa de Lima <111152377+kinhoown@users.noreply.github.com>
2024-06-06 15:52:07 -0300

    Update main.py

--- RPG/main.py
+++ RPG/main.py
@@ -2,9 +2,11 @@ from npcs import *
 from player import *
 
 quantidade_npcs(5)  #quantos npcs vai ser sumonado
-for npc in lista_npcs:
-    print(f"{npc['nome']} \\\\ Level: {npc['lvl']} \\\\ HP: {npc['hp']} \\\\ DANO: {npc['dano']}")
+for indice ,npc in enumerate(lista_npcs):
+    print(f"Monstro {indice} - {npc['nome']} \\\\ Level: {npc['lvl']} \\\\ HP: {npc['hp']} \\\\ DANO: {npc['dano']}")
 
 
-for i , v in avatar.items():
-    print(f'{i} : {v}')
+'''for i , v in avatar.items():
+    print(f'{i} : {v}')'''  #mostrar avatar
+
+escolher_monstro_ataque = int(input('Qual o mostro vc deseja atacar? '))
